fb033acac55871edb730a2467f280b9e9240ecf0
Brett Derham <brettderham@Bretts-MacBook-Pro.local>
2018-12-07 15:06:02 -0800

    Change finished

--- homework05/haskell/Warmup.hs
+++ homework05/haskell/Warmup.hs
@@ -1,1 +1,23 @@
+module Warmup
+  (change
+  , stripQuotes)
+  where
 
+
+change :: Int
+change amount =
+    if amount < 0 then
+        Left "Amount can be negative"
+    else
+        let
+            (quarter, afterQuarters) = amount 'divMod' 25
+            (dimes, afterDimes) = afterQuarters 'divMod' 10
+            (nickels, pennies) = afterDimes 'divMod' 5
+          in
+        Right (quarters, dimes, nickels, pennies)
+
+
+
+
+powers :: Double
+powers value =
