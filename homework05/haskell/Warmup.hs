53a7858982f7d911c3f616e4a9dde55ffadc5f64
Brett Derham <brettderham@Bretts-MacBook-Pro.local>
2018-12-08 16:31:31 -0800

    Almost done just need c and d

--- homework05/haskell/Warmup.hs
+++ homework05/haskell/Warmup.hs
@@ -1,8 +1,12 @@
 module Warmup
   (change
   , stripQuotes
-  -- , sumOfCubesOfOdds
-  , swapAdjacents)
+--   , powers
+  , sumOfCubesOfOdds
+  , swapAdjacents
+  , Shape (Box, Sphere)
+  , volume
+  , surfaceArea)
   where
 
 
@@ -20,10 +24,29 @@ change amount =
 
 stripQuotes s = filter (\c -> c /= '\'' && c /= '"') s
 
--- sumOfCubesOfOdds :: Int -> Int -> [a] -> [a]
--- sumOfCubesOfOdds (h:t) : if (not . even) h then h*h*h + (sumOfCubesOfOdds t) else sumOfCubesOfOdds t
 
+-- powers = 1 : map double powers
+--    where double n = n*(n+1)
+
+sumOfCubesOfOdds :: Integral n => [n] -> n
+sumOfCubesOfOdds [] = 0
+sumOfCubesOfOdds (x:xs)
+    | odd x = x^3 + sumOfCubesOfOdds xs
+    | otherwise = sumOfCubesOfOdds xs
 
 swapAdjacents [] = []
 swapAdjacents xs@[_] = xs
 swapAdjacents (a : b : xs) = b : a : swapAdjacents xs
+
+data Shape
+    = Sphere Double
+    | Box Double Double Double
+    deriving (Eq, Show)
+
+volume :: Shape -> Double
+volume (Sphere r) = ( 4 / 3 ) * pi * r * r * r
+volume (Box h l w) = h * l * w
+
+surfaceArea :: Shape -> Double
+surfaceArea (Sphere r) = 4 * pi * r * r
+surfaceArea (Box h l w) = (2 * h * w) + (2 * h * l) + (2 * l *w)
